f41abfc922c61b79f6aeb685b6e6db76daca9ce2
Evgeny <evgenusov96@gmail.com>
2023-03-13 16:29:35 +0200

    chore: initial commit

---
+++ android/src/newarch/ProximitySensorSpec.kt
@@ -0,0 +1,7 @@
+package com.proximitysensor
+
+import com.facebook.react.bridge.ReactApplicationContext
+
+abstract class ProximitySensorSpec internal constructor(context: ReactApplicationContext) :
+  NativeProximitySensorSpec(context) {
+}
